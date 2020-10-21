7ab3baf5fa4ed06da436debd81c42cfe24279557
wangtingjun <wangtingjun2018@gmail.com>
2020-10-21 21:33:29 +0800

    Add a new file named test01.py

---
+++ test01.py
@@ -0,0 +1,22 @@
+def asteroidCollision(asteroids):
+    res = []
+    for char in asteroids:
+        if char < 0 and not res:
+            res.append(char)
+        elif char > 0:
+            res.append(char)
+        elif char < 0 and res:
+            answer = False
+            while res[-1] > 0:
+                if res[-1] < abs(char):
+                    res.pop()
+                if res[-1] == abs(char):
+                    res.pop()
+                    break
+                else:
+                    break
+            if answer == True:
+                res.append(char)
+    return res
+
+print(asteroidCollision([-1,2,-1,2]))
